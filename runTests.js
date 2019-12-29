30624f61a2d3e0755039c4b456b5ff1fd603efe8
Mohamed IDRISSI <med.idrissi@outlook.com>
2019-12-29 14:40:45 +0100

    chore(deps): update dependencies

--- runTests.js
+++ runTests.js
@@ -1,13 +1,19 @@
-var spawnSync = require('child_process').spawnSync;
-var spawn = require('child_process').spawn;
-var testServer = require('./test/connect/server');
+const { spawnSync } = require('child_process');
+const { spawn } = require('child_process');
+const testServer = require('./test/connect/server');
 
 testServer.start(function () {
-    spawnSync('npm run build', { shell: true, stdio: 'inherit' });
-    var testProcess = spawn('npm run test:all', { shell: true, stdio: 'inherit' });
-    testProcess.on('close', (code) => {
-        testServer.stop(function () {
-            process.exitCode = code;
-        });
+  spawnSync('npm run build', {
+    shell: true,
+    stdio: 'inherit',
+  });
+  const testProcess = spawn('npm run test:all', {
+    shell: true,
+    stdio: 'inherit',
+  });
+  testProcess.on('close', (code) => {
+    testServer.stop(function () {
+      process.exitCode = code;
     });
+  });
 });
